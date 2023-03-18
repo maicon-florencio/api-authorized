a73e2de72376c764742c6bbf9f10d7201b4661ed
maiconsflorencio <mflorencio-ext@farmtech.com.br>
2023-03-18 16:57:10 -0300

    Conclusao de teste unitario para o mapper userCustom

--- src/test/java/com/example/demo/builder/UserCustomBuilderTest.java
+++ src/test/java/com/example/demo/builder/UserCustomBuilderTest.java
@@ -4,6 +4,9 @@ import com.example.demo.dominio.UserCustom;
 import com.example.demo.service.DTO.UserCustomDTO;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Component
 public class UserCustomBuilderTest {
 
@@ -26,4 +29,50 @@ public class UserCustomBuilderTest {
                 .enabled(Boolean.TRUE)
                 .build();
     }
+
+
+    public List<UserCustom> returnListUserOkAtivo(){
+        return Arrays.asList( UserCustom.builder()
+                .idUser(1L)
+                .firstName("JOSE")
+                .fullName("S Quieroz")
+                .password("banana")
+                .enabled(Boolean.TRUE)
+                    .build(), UserCustom.builder()
+                    .idUser(2L)
+                    .firstName("JOAQUIM")
+                    .fullName("S AZEVEDO")
+                    .password("CABRIOXA")
+                    .enabled(Boolean.TRUE)
+                    .build(), UserCustom.builder()
+                    .idUser(3L)
+                    .firstName("PEDRO")
+                    .fullName("ALVEZ")
+                    .password("PERA")
+                    .enabled(Boolean.TRUE)
+                    .build());
+    }
+
+    public List<UserCustomDTO> returnListUserDtoOkAtivo(){
+        return Arrays.asList( UserCustomDTO.builder()
+                .idUser(1L)
+                .firstName("JOSEdto")
+                .fullName("S Quieroz dto")
+                .password("banana")
+                .enabled(Boolean.TRUE)
+                .build(), UserCustomDTO.builder()
+                .idUser(2L)
+                .firstName("JOAQUIM dto")
+                .fullName("S AZEVEDO dto")
+                .password("CABRIOXA")
+                .enabled(Boolean.TRUE)
+                .build(), UserCustomDTO.builder()
+                .idUser(3L)
+                .firstName("PEDRO dto")
+                .fullName("ALVEZ dto")
+                .password("PERA")
+                .enabled(Boolean.TRUE)
+                .build());
+    }
+
 }
--- src/test/java/com/example/demo/mapper/UserCustomMapperTest.java
+++ src/test/java/com/example/demo/mapper/UserCustomMapperTest.java
@@ -1,10 +1,15 @@
 package com.example.demo.mapper;
 
 import com.example.demo.builder.UserCustomBuilderTest;
+import com.example.demo.dominio.UserCustom;
+import com.example.demo.service.DTO.UserCustomDTO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Collection;
+import java.util.List;
 
 @SpringBootTest
 public class UserCustomMapperTest {
@@ -30,6 +35,20 @@ public class UserCustomMapperTest {
         Assertions.assertEquals(user.getFullName(),"Sanchez");
         Assertions.assertEquals(user.getPassword(),"Cocacola");
 
+    }
+    @Test
+    public void testListUserDTOParseUSer(){
+        var entities =  UserCustomMapper.INSTANCE.toEntites(builderUser.returnListUserDtoOkAtivo());
+
+        Assertions.assertTrue(entities instanceof List<UserCustom>);
+        Assertions.assertTrue(entities.get(0).getFirstName().equals("JOSEdto"));
+    }
+    @Test
+    public void testListUserParseUserDto(){
+        var dtos =  UserCustomMapper.INSTANCE.toDtos(builderUser.returnListUserOkAtivo());
+
+        Assertions.assertTrue(dtos instanceof Collection<?>);
+        Assertions.assertTrue(dtos.get(0).getFirstName().equals("JOSE"));
     }
 
 
