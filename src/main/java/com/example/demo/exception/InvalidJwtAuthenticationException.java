c4f9c5a58c85dcca0b223e9d6d3de247490baa89
maiconsflorencio <mflorencio-ext@farmtech.com.br>
2023-04-17 10:53:27 -0300

    criacao de exeception para autorizacao

---
+++ src/main/java/com/example/demo/exception/InvalidJwtAuthenticationException.java
@@ -0,0 +1,12 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class InvalidJwtAuthenticationException extends AuthenticationException {
+    public InvalidJwtAuthenticationException(String msg){
+        super(msg);
+    }
+}
