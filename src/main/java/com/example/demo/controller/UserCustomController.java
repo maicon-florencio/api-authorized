49b98ef0744d3a1b793c29c94b9320dfe80be5b3
maiconsflorencio <mflorencio-ext@farmtech.com.br>
2023-06-11 19:57:07 -0300

    Criacao de apontamento swagger para o endpoint junto com documentacao

--- src/main/java/com/example/demo/controller/UserCustomController.java
+++ src/main/java/com/example/demo/controller/UserCustomController.java
@@ -2,6 +2,12 @@ package com.example.demo.controller;
 
 import com.example.demo.service.Dto.UserCustomDTO;
 import com.example.demo.service.UserCustomService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/api/user")
+@Tag(name="People", description = " Endpoints relationship People")
 public class UserCustomController {
     @Autowired
     private UserCustomService userCustomService;
@@ -22,6 +29,15 @@ public class UserCustomController {
 
     }
     @GetMapping("/{id}")
+    @Operation(summary = "Finds person from ID", description = "Finds person from ID", responses = {
+            @ApiResponse(description = "Sucess", responseCode = "200", content = @Content(
+                    mediaType = "application/json",array = @ArraySchema(schema = @Schema(implementation = UserCustomDTO.class))
+            )),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+    })
     public ResponseEntity<UserCustomDTO> findById(@PathVariable("id") Long id){
         log.info("Chamada de buscar user por ID");
         return ResponseEntity.ok(userCustomService.findById(id));
